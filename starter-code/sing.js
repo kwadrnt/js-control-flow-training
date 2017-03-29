e01bb2fb768d34b14034f22317620ca1c7b7c2fa
kwadrnt <kevinctse@gmail.com>
2017-03-29 00:28:03 -0700

    singjs working

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,19 @@
 console.log("sing.js is loaded");
+
+let numBottles = prompt("How many bottles of beer on the wall?");
+
+let bottles = "bottles";
+while (numBottles > 0){
+	console.log(`${numBottles} ${bottles}  of beer on the wall,`);
+	console.log(`${numBottles} ${bottles}  of beer`);
+	console.log("Take one down and pass it around,");
+	numBottles = numBottles - 1;
+	if (numBottles === 1){
+		bottles = "bottle"
+	}
+	if (numBottles === 0){
+		console.log("No more bottles of beer on the wall!");
+	} else {
+		console.log(`${numBottles} ${bottles}  of beer on the wall!`);
+	}
+}
