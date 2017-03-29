a9048b9aa6d881835a98817d5e65f15c36440717
kwadrnt <kevinctse@gmail.com>
2017-03-29 00:25:13 -0700

    completed security and login

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,21 @@
 console.log("login.js is loaded");
+
+
+const userLogin = {
+  userName: "octocat_rules",
+  password: "abacadabra"
+};
+
+let passwordInput;
+
+let loggedIn = false;
+
+while (!loggedIn){
+	passwordInput = prompt(`Enter password for user ${userLogin.userName}.`)
+	if (passwordInput === userLogin.password){
+		console.log("Passwords match!");
+    loggedIn = true;
+	} else {
+		console.log("Passwords do not match. Try Again!");
+	}
+}
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,33 @@
 console.log("security_questions.js is loaded");
+
+// ES5 old school JS!
+var securityQuestions = [
+	{
+		question: "What is your favorite food?",
+		expectedAnswer: "Chinese"
+	},
+	{
+		question: "What is your birth city?",
+		expectedAnswer: "Los Angeles"
+	},
+	{
+		question: "What street did you grow up on?",
+		expectedAnswer: "Eastern"
+	}
+]
+
+var userAnswer = "";
+var hacker = false;
+
+for (var i=0; i < securityQuestions.length; i++){
+	userAnswer = prompt(securityQuestions[i].question);
+	if (userAnswer !== securityQuestions[i].expectedAnswer){
+		alert("Incorrect security question response!");
+    hacker = true;
+		break;
+	}
+}
+
+if (hacker){
+  console.log("Stop!");
+}
